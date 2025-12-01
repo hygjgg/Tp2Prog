a4ec36564c7e690fc15581a7dcfe883de1f1076c
Franco <aguilarfranco2019@gmail.com>
2025-12-01 15:46:20 -0300

    Update Estudiante.java

--- Estudiante.java
+++ Estudiante.java
@@ -8,25 +8,33 @@ public class Estudiante {
         this.materias = new ListaEnlazada();
     }
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public ListaEnlazada getMaterias() {
+        return materias;
+    }
+
     public void agregarMateria(Materia m) {
         materias.agregar(m);
     }
 
-    // Método recursivo adaptado a nodos
+    // Método recursivo que inicia desde el primer nodo
     public double calcularPromedio() {
-        return calcularPromedioRecursivo(materias.getPrimero(), 0, 0);
+        return calcularPromedioRecursivo(materias.getPrimero(), 0, 0.0);
     }
 
     public static double calcularPromedioRecursivo(Nodo actual, int contador, double suma) {
         if (actual == null) {
-            return contador == 0 ? 0 : suma / contador;
+            return contador == 0 ? 0.0 : suma / contador;
         }
-
         Materia mat = (Materia) actual.dato;
-        return calcularPromedioRecursivo(
-            actual.siguiente,
-            contador + 1,
-            suma + mat.getNota()
-        );
+        return calcularPromedioRecursivo(actual.siguiente, contador + 1, suma + mat.getNota());
+    }
+
+    @Override
+    public String toString() {
+        return nombre + " - Promedio: " + String.format("%.2f", calcularPromedio());
     }
 }
